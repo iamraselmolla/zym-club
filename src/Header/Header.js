55cbfcfc076060633c53b37692bcbf0d88103c7b
iamraselmolla <raselmolla6336@gmail.com>
2022-09-28 20:56:27 +0600

    Header designedt

---
+++ src/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css';
+import logo from '../logo.png'
+
+const Header = () => {
+    return (
+        <header>
+            <div className="main-header">
+                <img src={logo} width='200px' alt="" />
+                <h1 className="logo-tag-line">
+                    Zym Club
+                </h1>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
